8c4d89ef8ab28deb24a4152bdaab65add72794ca
Cassiano Menezes <cassiano.menezes@MSAODXTCPS000001.local>
2020-11-18 16:05:01 -0300

    Adding Koin.

---
+++ app/src/main/java/com/cassianomenezes/gifapp/di/KoinModule.kt
@@ -0,0 +1,28 @@
+package com.cassianomenezes.gifapp.di
+
+import com.cassianomenezes.gifapp.home.view.viewmodel.MainViewModel
+import com.cassianomenezes.gifapp.network.RetrofitClient
+import com.cassianomenezes.gifapp.repository.DataRepository
+import org.koin.androidx.viewmodel.dsl.viewModel
+import org.koin.dsl.module
+
+private val serviceModule = module {
+    single { RetrofitClient.getApiService() }
+}
+
+private val repositoryModule = module {
+    single { DataRepository(service = get()) }
+}
+
+/*private val resourceManager = module {
+    single { ResourceManager(context = get()) }
+}*/
+
+private val viewModelModule = module {
+    viewModel { MainViewModel(/*repository = get()*/) }
+    //viewModel { RecipeDetailViewModel(resourceManager = get()) }
+}
+
+fun loadKoinModules() {
+    org.koin.core.context.loadKoinModules(listOf(serviceModule, repositoryModule, /*resourceManager,*/ viewModelModule))
+}
--- app/src/main/java/com/cassianomenezes/gifapp/home/view/activity/MainActivity.kt
+++ app/src/main/java/com/cassianomenezes/gifapp/home/view/activity/MainActivity.kt
@@ -2,11 +2,27 @@ package com.cassianomenezes.gifapp.home.view.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import com.cassianomenezes.gifapp.BR
 import com.cassianomenezes.gifapp.R
+import com.cassianomenezes.gifapp.extension.bindingContentView
+import com.cassianomenezes.gifapp.home.view.viewmodel.MainViewModel
+import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class MainActivity : AppCompatActivity() {
+
+    private val viewModel: MainViewModel by viewModel()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        //setContentView(R.layout.activity_main)
+        setupBinding()
+    }
+
+    private fun setupBinding() {
+        bindingContentView(R.layout.activity_main).apply {
+            setVariable(BR.viewModel, viewModel)
+            //setVariable(BR.onTryAgainClick, View.OnClickListener { onTryAgainClick() })
+        }
     }
 }
